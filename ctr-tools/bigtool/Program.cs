ffa23147ab20b8e7b1bb1d9937127597c061909c
DCxDemo <ultrataber@gmail.com>
2020-04-26 21:06:17 +0300

    bigtool code migration WIP
    big handling code is fully migrating to CTRFramework. not fully implemented yet.

--- ctr-tools/bigtool/Program.cs
+++ ctr-tools/bigtool/Program.cs
@@ -1,73 +1,41 @@
 ﻿using System;
 using System.IO;
-using p = bigtool.Properties.Resources;
+using CTRFramework;
+using CTRFramework.Shared;
 
 namespace bigtool
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            //Thread.CurrentThread.CurrentCulture = new CultureInfo("en");
-            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
-
-            Greet();
+            Console.WriteLine(
+                "{0}\r\n{1}\r\n\r\n{2}\r\n",
+                "CTR-tools: BigTool",
+                "Builds and extracts Crash Team Racing BIG files",
+                "CTRFramework " + Meta.GetVersion() );
 
             if (args.Length > 0)
             {
                 string ext = Path.GetExtension(args[0]).ToLower();
 
+                BIG2 big = new BIG2(args[0]);
+
                 switch (ext)
                 {
-                    case ".big": Extract(args[0]); break;
-                    case ".txt": Build(args[0]); break;
-                    default: Console.WriteLine("{0}: {1}", p.file_not_supported, ext); break;
+                    case ".big": big.Export(".\\bigfile"); break;
+                    //case ".txt": Build(); break;
+                    default: Console.WriteLine("{0}: {1}", "Unsupported file", ext); break;
                 }
 
-                //Console.ReadKey();
                 return;
             }
 
-            Info();
-            //Console.ReadKey();
-        }
-
-
-        static void Extract(string path)
-        {
-            BIG big = new BIG(path);
-            big.Export();
-            Console.WriteLine(p.done);
-        }
-
-
-        static void Build(string path)
-        {
-            BIG big = new BIG();
-            big.Build(path);
-            Console.WriteLine(p.done);
-        }
-
-
-        static void Greet()
-        {
             Console.WriteLine(
-                "{0}\r\n{1}\r\n\r\n{2}\r\n",
-                p.project_name,
-                p.app_desc,
-                p.copyright);
-        }
-
-
-        static void Info()
-        {
-            Console.WriteLine(
-               "{0}:\r\n\t{1}: {2}\r\n\t{3}: {4}\r\n\r\n{5}",
-               p.usage,
-               p.split_example, "bigtool C:\\BIGFILE.BIG",
-               p.merge_example, "bigtool C:\\BIGFILE.TXT",
-               p.confirm_quit
+               "{0}:\r\n\t{1}: {2}\r\n\t{3}: {4}",
+               "Usage",
+               "Split example", "bigtool C:\\BIGFILE.BIG",
+               "Merge example", "bigtool C:\\BIGFILE.TXT"
             );
         }
     }
--- ctr-tools/bigtool/Properties/AssemblyInfo.cs
+++ ctr-tools/bigtool/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
-// связанные с этой сборкой.
 [assembly: AssemblyTitle("bigtool")]
 [assembly: AssemblyDescription("Extracts and builds BIG container, found in Crash Team Racing (PS1)")]
 [assembly: AssemblyConfiguration("")]
@@ -12,24 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("dcxdemo")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// из модели COM задайте для атрибута ComVisible этого типа значение true.
 [assembly: ComVisible(false)]
-
-// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
 [assembly: Guid("43f67739-b59b-49cf-a22a-f487ab1157e4")]
-
-// Сведения о версии сборки состоят из указанных ниже четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//      Номер сборки
-//      Номер редакции
-//
-// Можно задать все значения или принять номера сборки и редакции по умолчанию 
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
