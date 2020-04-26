430405cf2fbb4fae295bb8b11257bae3f583f544
DCxDemo <ultrataber@gmail.com>
2020-04-26 21:09:58 +0300

    Bigfile migration
    - ctrfile -> bigentry
    - big is the old big class
    - big2 is the new big class, the previous one will be removed when second fully implements original features.
    
    the rewritten version will drop some stuff like extension assumption, cause it's not needed anymore since we have all the files named properly.

--- ctr-tools/CTRFramework/code/big/BIG.cs
+++ ctr-tools/CTRFramework/code/big/BIG.cs
@@ -1,85 +1,182 @@
-﻿using System;
+﻿using CTRFramework;
+using CTRFramework.Shared;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using CTRFramework.Shared;
+using p = CTRFramework.Properties.Resources;
 
-namespace CTRFramework
+namespace bigtool
 {
+    /*
+    public struct Pair
+    {
+        public uint size;
+        public uint offset;
+
+        public Pair(uint o, uint s)
+        {
+            offset = o;
+            size = s;
+        }
+    }
+    */
+
     public class BIG
     {
-        public List<CTRFile> files = new List<CTRFile>();
+        BinaryReaderEx br;
+        MemoryStream ms;
+
+        public int totalFiles = 0;
+
+        public List<BigEntry> pairs = new List<BigEntry>();
+
+        public Dictionary<int, string> names = new Dictionary<int, string>();
 
         public BIG()
         {
         }
 
 
+        public string path;
+        string basepath;
+        string filelist;
+        string bigname;
+        string bigpath;
+        string extpath;
+        string verpath;
+
+        public void InitPaths(string fn)
+        {
+            path = fn;
+            basepath = System.AppDomain.CurrentDomain.BaseDirectory;
+            filelist = Path.Combine(basepath, "filenames_usa.txt");
+            bigname = Path.GetFileNameWithoutExtension(fn);
+            bigpath = Path.GetDirectoryName(fn);
+            if (bigpath == null || bigpath == "") bigpath = basepath;
+            extpath = Path.Combine(bigpath, bigname) + "\\";
+            verpath = Path.Combine(basepath, "versions.json");
+
+            //uncomment if something goes wrong
+            /*
+            Console.WriteLine(path);
+            Console.WriteLine(basepath);
+            Console.WriteLine(filelist);
+            Console.WriteLine(bigname);
+            Console.WriteLine(bigpath);
+            Console.WriteLine(extpath);
+            */
+        }
+
+
         public BIG(string fn)
         {
             Console.WriteLine("Begin: " + fn);
+            InitPaths(fn);
 
-            //string mode = Meta.DetectBig(fn);
+            ms = new MemoryStream(File.ReadAllBytes(fn));
+            br = new BinaryReaderEx(ms);
 
-            using (BinaryReaderEx br = new BinaryReaderEx(File.OpenRead(fn)))
-            {
-                br.Jump(4);
-                uint totalFiles = br.ReadUInt32();
+            br.BaseStream.Position += 4;
+            totalFiles = br.ReadInt32();
 
-                for (int i = 0; i < totalFiles; i++)
-                {
-                    CTRFile cf = new CTRFile();
-                    cf.name = i.ToString("0000");
-                    cf.offset = br.ReadUInt32() * 2048;
-                    cf.size = br.ReadUInt32();
+            var namelist = Meta.LoadNumberedList(Meta.Detect(fn, "bigs", totalFiles));
+            Console.WriteLine("File list ({0}) - {1}\r\n", namelist, (namelist != null ? "OK" : "ERROR"));
 
-                    files.Add(cf);
-                }
-
-                foreach (CTRFile cf in files)
-                {
-                    br.Jump(cf.offset);
-                    cf.data = br.ReadBytes((int)cf.size);
-                }
-
-                LoadNames("filenames.txt");
-            }
+            for (int i = 0; i < totalFiles; i++)
+                pairs.Add(new BigEntry(br, (names.ContainsKey(i) ? names[i] : i.ToString("000")), i));
         }
 
-        public void Export(string path)
+
+        StringBuilder biglist = new StringBuilder();
+
+        public void Export()
         {
-            StringBuilder biglist = new StringBuilder();
+            int i = 0;
 
-            Console.Write(files.Count + " files:\r\n");
+            Directory.CreateDirectory(extpath);
+            Console.Write(pairs.Count + " files:\r\n");
 
-            foreach (CTRFile cf in files)
+            foreach (BigEntry p in pairs)
             {
+                //detect known formats
 
-                string filename = Path.Combine(path, cf.name);
-                string dirname = Path.GetDirectoryName(filename);
+                br.BaseStream.Position = p.Offset * 2048;
 
-                try
+                uint h = br.ReadUInt32();
+                uint h2 = br.ReadUInt32();
+
+                string knownext = "";
+
+                switch (h)
                 {
-                    Directory.CreateDirectory(dirname);
+                    case 0x00000010: if (h2 == 2) knownext = ".tim"; break;
+                    case 0x00000020: knownext = ".vram"; break;
+                    case 0x80010160: knownext = ".str"; break;
+                        //default: knownext = ""; break;
                 }
-                catch
+
+                if (p.Size == 0) knownext = ".null";
+
+                //--------------
+
+                br.BaseStream.Position = p.Offset * 2048;
+
+                string knownname = "";
+
+                if (names.ContainsKey(i))
+                    knownname = names[i];
+
+
+                string fname =
+                    extpath +
+                    i.ToString("0000") +
+                    (knownname != "" ? ("_" + knownname) : "") +
+                    knownext;
+
+                /*
+                biglist.Append(i.ToString("0000") +
+                    (knownname != "" ? ("_" + knownname) : "") +
+                    knownext + "\r\n");
+                */
+
+
+                if (knownname == "") knownname = i.ToString("0000") + knownext;
+
+                if (!knownname.Contains("\\")) knownname = i.ToString("0000") + "_" + knownname;
+
+                byte[] data = br.ReadBytes((int)p.Size);
+
+                //string filemd5 = CalculateMD5(data);
+
+                //if (Path.GetExtension(knownname) == ".lev")
+                // biglist.Append(filemd5 + "\t" + "NTSC-J\t" + knownname + "\r\n");
+
+                biglist.Append(knownname + "\r\n");
+
+                fname = Path.Combine(extpath, knownname);
+
+
+                if (p.Size > 0)
                 {
-
+                    Directory.CreateDirectory(Path.GetDirectoryName(fname));
+                    File.WriteAllBytes(fname, data);
                 }
 
-                biglist.AppendLine(cf.name);
-                if (cf.size > 0) File.WriteAllBytes(filename, cf.data);
-                
-                //Console.Write(".");
+                Console.Write(".");
+
+                i++;
+
+                //Console.WriteLine(fname);
             }
 
-            File.WriteAllText("bigfile.txt", biglist.ToString());
+            File.WriteAllText(bigname + ".txt", biglist.ToString());
 
             Console.Write("\r\n");
+
         }
 
 
-        /*
         public void Build(string txt)
         {
             string path = Path.GetFileNameWithoutExtension(txt);
@@ -91,91 +188,65 @@ namespace CTRFramework
             for (int i = 0; i < files.Length; i++)
             {
                 files[i] = ".\\" + path + "\\" + files[i];
-                ctrfiles.Add(new CTRFile(files[i]));
+                pairs.Add(new BigEntry(files[i]));
             }
 
-            Console.WriteLine("we'll need " + TotalSize() + " Bytes for " + ctrfiles.Count + " files:");
+            Console.WriteLine("we'll need " + TotalSize() + " Bytes for " + pairs.Count + " files:");
 
             byte[] final_big = new byte[TotalSize()];
             MemoryStream ms = new MemoryStream(final_big);
             BinaryWriterEx bw = new BinaryWriterEx(ms);
 
             bw.BaseStream.Position = 4;
-            bw.Write(ctrfiles.Count);
+            bw.Write(pairs.Count);
 
             bw.BaseStream.Position = 3 * 2048;
 
-            foreach (CTRFile c in ctrfiles)
+            foreach (BigEntry c in pairs)
             {
                 Console.Write(".");
 
-                uint pos = (uint)bw.BaseStream.Position;
-                c.offset = pos / 2048;
+                int pos = (int)bw.BaseStream.Position;
+                c.Offset = pos / 2048;
 
 
-                bw.Write(c.data);
+                bw.Write(c.Data);
 
-                bw.BaseStream.Position = pos + c.padded_size;
+                bw.BaseStream.Position = pos + c.SizePadded;
             }
 
             Console.WriteLine();
 
             bw.BaseStream.Position = 8;
 
-            foreach (CTRFile c in ctrfiles)
+            foreach (BigEntry c in pairs)
             {
-                bw.Write(c.offset);
-                bw.Write(c.size);
+                bw.Write(c.Offset);
+                bw.Write(c.Size);
             }
 
-            Console.WriteLine(p.disk_dump);
-
-            bw.Close();
-            ms.Close();
+            Console.WriteLine("Dumping to disk:");
 
             File.WriteAllBytes(path + ".BIG", final_big);
 
-            Console.WriteLine(p.big_created);
+            Console.WriteLine("BIG file successfully created.");
 
         }
 
-    */
 
-        public uint TotalSize()
+
+        public int TotalSize()
         {
             //let's hardcode it as you can't add files anyway.
             //ideally you should calculate the amount of sectors used for size/offset array
-            uint to_alloc = 2048 * 3;
+            int to_alloc = 2048 * 3;
 
-            foreach (CTRFile c in files)
-                to_alloc += c.padded_size;
+            foreach (BigEntry c in pairs)
+                to_alloc += c.SizePadded;
 
             return to_alloc;
         }
 
-        private void LoadNames(string path)
-        {
-            string[] buf = File.ReadAllLines(path);
 
-            foreach (string b in buf)
-            {
-                if (b.Trim() != "")
-                {
-                    string[] bb = b.Replace(" ", "").Split('=');
-
-                    int x = -1;
-                    Int32.TryParse(bb[0], out x);
-
-                    if (x == -1)
-                    {
-                        Console.WriteLine("List parsing error at: {0}", bb);
-                        continue;
-                    }
-
-                    files[x].name = bb[1];
-                    //names.Add(x, bb[1]);
-                }
-            }
-        }
     }
 }
---
+++ ctr-tools/CTRFramework/code/big/BigEntry.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using CTRFramework.Shared;
+
+namespace CTRFramework
+{
+    public class BigEntry
+    {
+        public int Index;
+        public string Name;
+        public int Size;
+        public int Offset;
+        public int SizePadded
+        {
+            get
+            {
+                int val = Size;
+                while (SizePadded % 2048 != 0) val++;
+                return val;
+            }
+        }
+
+        public byte[] Data;
+
+        public BigEntry()
+        {
+        }
+
+        /// <summary>
+        /// Reads indexed Big Entry from BigFile
+        /// </summary>
+        /// <param name="br">Source stream</param>
+        /// <param name="name">Entry name</param>
+        /// <param name="i">Entry index</param>
+        public BigEntry(BinaryReaderEx br, string name, int i)
+        {
+            Index = i;
+            Name = name;
+            br.Jump(4);
+
+            if (Index >= br.ReadInt32() || Index < 0)
+                throw new ArgumentOutOfRangeException("Big entry index out of range.");
+
+            br.Skip(i * 8);
+
+            Offset = br.ReadInt32() * Meta.SectorSize;
+            Size = br.ReadInt32();
+
+            if (Offset + Size > br.BaseStream.Length)
+            {
+                Console.WriteLine("Attempt to read data beyond binary stream.");
+                throw new ArgumentOutOfRangeException("Attempt to read data beyond binary stream.");
+            }
+
+            br.Jump(Offset);
+            Data = br.ReadBytes(Size);
+        }
+
+        /// <summary>
+        /// Creates Big Entry from an external file
+        /// </summary>
+        /// <param name="path">Path to file</param>
+        public BigEntry(string path, string name = null)
+        {
+            Name = name != null ? name : path;
+            Data = new byte[0];
+            Size = 0;
+
+            if (File.Exists(path))
+            {
+                try
+                {
+                    Data = File.ReadAllBytes(path);
+                    Size = Data.Length;
+                }
+                catch
+                {
+                    Console.WriteLine("Unable to access the file.");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name + " [" + ((Size / 1024) + 1) + "kb]";
+        }
+    }
+}
