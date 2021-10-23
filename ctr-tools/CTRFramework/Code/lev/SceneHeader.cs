917fc88e5add64fd2db2d735dcb6aef1d67afd5b
DCxDemo <ultrataber@gmail.com>
2021-10-23 14:36:55 +0300

    updated sceneheader comments
    updated sceneheader comments

--- ctr-tools/CTRFramework/Code/lev/SceneHeader.cs
+++ ctr-tools/CTRFramework/Code/lev/SceneHeader.cs
@@ -4,74 +4,79 @@ using System.Collections.Generic;
 
 namespace CTRFramework
 {
-    public class SceneHeader : IRead
+    public class SceneHeader : IReadWrite
     {
         public static readonly int SizeOf = 0x1B0;
 
-        public PsxPtr ptrMeshInfo;      //0x0 - pointer to MeshInfo
-        public PsxPtr ptrSkybox;        //0x4 - pointer to SkyBox
-        public PsxPtr ptrAnimTex;       //0x8 - pointer to animated textures array
+        public PsxPtr ptrMeshInfo;      //0x00 - pointer to MeshInfo
+        public PsxPtr ptrSkybox;        //0x04 - pointer to SkyBox
+        public PsxPtr ptrAnimTex;       //0x08 - pointer to animated textures array
 
-        public int numInstances;        //0xC - number of model instances in the level (i.e. every single box, fruit, etc.)
-        public PsxPtr ptrInstances;    //0x10 - points to the 1st entry of the array of model instances
+        public int numInstances;        //0x0C - number of model instances in the level (i.e. every single box, fruit, etc.)
+        public PsxPtr ptrInstances;     //0x10 - points to the 1st entry of the array of model instances
         public int numModels;           //0x14 - number of actual models
-        public PsxPtr ptrModelsPtr;    //0x18 - pointer to the array of pointers to models. easy in c++, messy in c# 
+        public PsxPtr ptrModelsPtr;     //0x18 - pointer to the array of pointers to models. easy in c++, messy in c# 
 
-        public uint unkPtr1;            //0x1C
-        public uint unkPtr2;            //0x20
-        public PsxPtr ptrInstancesPtr;    //0x24 - pointer to the array of pointers to model instances.
-        public uint unkPtr3;            //0x28
+        public uint unkPtr1;            //0x1C - unknown, extra visdata region
+        public uint unkPtr2;            //0x20 - unknown, extra visdata region
+        public PsxPtr ptrInstancesPtr;  //0x24 - pointer to the array of pointers to model instances.
+        public uint unkPtr3;            //0x28 - unknown, extra visdata region
 
-        public int null1;           //0x2C - assumed reserved
-        public int null2;           //0x30 - assumed reserved
+        public int null1;               //0x2C - assumed reserved
+        public int null2;               //0x30 - assumed reserved
 
-        public uint numWater;       //0x34 - number of vertices treated as water
-        public PsxPtr ptrWater;       //0x38 - pointer to array of water entries
-        public PsxPtr ptrIcons;       //0x3C - lead to the icon pack header
-        public PsxPtr ptrIconsArray;  //0x40 - leads to the icon pack data
-        public PsxPtr ptrRestartMain; //0x44 - looks like a restart point, but doesn't affect anything? maybe like play area bbox?
+        public uint numWater;           //0x34 - number of vertices treated as water
+        public PsxPtr ptrWater;         //0x38 - pointer to array of water entries
+        public PsxPtr ptrIcons;         //0x3C - lead to the icon pack header
+        public PsxPtr ptrIconsArray;    //0x40 - leads to the icon pack data
+        public PsxPtr ptrRestartMain;   //0x44 - looks like a restart point, but doesn't affect anything? maybe like play area bbox?
 
-        public Gradient[] glowGradients; //0x48 - renders glowing effect in papu's pyramid
-        public Pose[] startGrid;    //0x6C - array of 8 starting locations (96 bytes = (6 * 2) * 8)
+        public Gradient[] glowGradients;    //0x48 - used for additional skybox gradient (like papu's pyramid) (24 bytes = 3 * (2 + 2 + 4))
+        public Pose[] startGrid;        //0x6C - array of 8 starting locations (96 bytes = (6 * 2) * 8)
 
-        public uint unkPtr4;        //0xCC
-        public uint unkPtr5;        //0xD0
-        public PsxPtr ptrLowTexArray; //0xD4 - assumed to be a pointer to low textures array, there is no number of entries though
-        public Vector4b backColor;  //0xD8 - base background color, used to clear the screen
-        public uint bgMode;         //0xDC - control background drawing mode, 1 color, 2 colors, 4 colors
+        public uint unkPtr4;            //0xCC - unknown, extra visdata region
+        public uint unkPtr5;            //0xD0 - unknown, extra visdata region
 
-        public PsxPtr ptrBuildStart;  //0xE0 - pointer to string, date, assumed visdata compilation start
-        public PsxPtr ptrBuildEnd;    //0xE4 - pointer to string, date, assumed visdata compilation end
-        public PsxPtr ptrBuildType;   //0xE8 - pointer to string, assumed build type
+        public PsxPtr ptrLowTexArray;   //0xD4 - assumed to be a pointer to low textures array, there is no number of entries though
 
-        byte[] skip;                //0xEC - (7*8 = 56 bytes) assumed to be related to particles, contains particle gravity value
+        public Vector4b backColor;      //0xD8 - base background color, used to clear the screen
+        public uint bgMode;             //0xDC - control background drawing mode, 1 color, 2 colors, 4 colors, gradient mode
+
+        public PsxPtr ptrBuildStart;    //0xE0 - pointer to string, date, assumed visdata compilation start
+        public PsxPtr ptrBuildEnd;      //0xE4 - pointer to string, date, assumed visdata compilation end
+        public PsxPtr ptrBuildType;     //0xE8 - pointer to string, assumed build type
+
+        byte[] skip;                    //0xEC - (7*8 = 56 bytes) assumed to be related to particles, contains particle gravity value
 
         public Vector4b particleColorTop;       //0x124 - controls bottom color of particles (i.e. snow)
         public Vector4b particleColorBottom;    //0x128 - controls bottom color of particles (i.e. snow)
         public uint particleRenderMode;         //0x12C - assumed to control how particles are drawn
 
-        public uint cntTrialData;   //0x130 - that's incorrect
-        public PsxPtr ptrTrialData;   //0x134 - pointer to additional data referred to as "trialdata" for now
-        public uint cntu2;          //0x138 - 
-        public uint ptru2;          //0x13C
-        public uint numSpawnPts;    //0x140
-        public PsxPtr ptrSpawnPts;    //0x144
-        public uint numRestartPts;  //0x148 - number of restart points in the level
-        public PsxPtr ptrRestartPts;  //0x14C - points to the 1st entry in restart points array
+        public uint cntTrialData;       //0x130 - that's incorrect
+        public PsxPtr ptrTrialData;     //0x134 - pointer to additional data referred to as "trialdata" for now
 
-        byte[] skip2;               //0x150 - 16 bytes
+        public uint cntu2;              //0x138 - 
+        public uint ptru2;              //0x13C
 
-        public Vector4b[] bgColor;  //0x160 - 4 * 4 = 16 bytes for 4 background colors.
-        public uint skip2_unkPtr;   //0x170 - 
+        public uint numSpawnPts;        //0x140
+        public PsxPtr ptrSpawnPts;      //0x144
 
-        public uint numVcolAnim;    //0x174 - number of animated vertices data
-        public PsxPtr ptrVcolAnim;    //0x178 - pointer to animated vertices data
+        public uint numRestartPts;      //0x148 - number of restart points in the level
+        public PsxPtr ptrRestartPts;    //0x14C - pointer to the array of restart points (used for cameras, mask grabs, warp orbs) 
 
-        byte[] skip23;              //0x17C - 12 bytes
+        byte[] skip2;                   //0x150 - 16 bytes
 
-        public PsxPtr ptrAiNav;       //0x188 - pointer to bot path data
+        public Vector4b[] bgColor;      //0x160 - 4 background colors used in 2/4 background color modes (16 bytes = 4 * 4)
+        public uint skip2_unkPtr;       //0x170 - 
 
-        byte[] skip3;               //0x18C - 36 bytes
+        public uint numVcolAnim;        //0x174 - number of animated vertices data
+        public PsxPtr ptrVcolAnim;      //0x178 - pointer to animated vertices data
+
+        byte[] skip23;                  //0x17C - 12 bytes
+
+        public PsxPtr ptrAiNav;         //0x188 - pointer to bot path data
+
+        byte[] skip3;                   //0x18C - 36 bytes
 
 
         public SceneHeader()
