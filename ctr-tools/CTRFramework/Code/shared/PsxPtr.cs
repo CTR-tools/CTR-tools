bbbaebcc5c2bc2c56ba8274b654e46dd31ddad1c
DCxDemo <ultrataber@gmail.com>
2021-10-23 11:02:15 +0300

    some progress for #74
    - added PsxPtr class that splits original pointer and extra bits.
    - added verfication tests for the class

---
+++ ctr-tools/CTRFramework/Code/shared/PsxPtr.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CTRFramework.Shared
+{
+    public enum HiddenBits
+    {
+        None = 0,
+        Bit0 = 1,
+        Bit1 = 2,
+        Both = 3
+    }
+
+    public class PsxPtr : IEquatable<PsxPtr>, IReadWrite
+    {
+        public static PsxPtr Zero = new PsxPtr(0);
+
+        public UIntPtr Address = UIntPtr.Zero;
+        public HiddenBits ExtraBits = HiddenBits.None;
+
+        private uint value => Address.ToUInt32() | (uint)ExtraBits;
+
+        public PsxPtr(uint val)
+        {
+            Convert(val);
+        }
+
+        public PsxPtr(BinaryReaderEx br)
+        {
+            Read(br);
+        }
+
+        /// <summary>
+        /// Reads the original pointer.
+        /// </summary>
+        /// <param name="br">BinaryReaderEx instance.</param>
+        public void Read(BinaryReaderEx br)
+        {
+            Convert(br.ReadUInt32());
+        }
+
+        public static PsxPtr FromReader(BinaryReaderEx br)
+        {
+            return new PsxPtr(br);
+        }
+
+        /// <summary>
+        /// Splits original value in MIPS pointer and extra hidden bits.
+        /// </summary>
+        /// <param name="val"></param>
+        private void Convert(uint val)
+        {
+            Address = (UIntPtr)(val >> 2 << 2);
+            ExtraBits = (HiddenBits)(val & 3);
+        }
+
+        /// <summary>
+        /// Writes pointer and extra bits to the target writer.
+        /// </summary>
+        /// <param name="bw">BinaryWriterEx instance.</param>
+        /// <param name="patchTable">Patch table to update.</param>
+        public void Write(BinaryWriterEx bw, List<UIntPtr> patchTable)
+        {
+            bw.Write((UIntPtr)value, patchTable);
+        }
+
+        public bool Equals(PsxPtr other)
+        {
+            return this.value == other.value;
+        }
+
+        public override string ToString()
+        {
+            return $"0x{Address.ToUInt32().ToString("X8")} [{ExtraBits}] <= ({value})";
+        }
+    }
+}
---
+++ ctr-tools/CTRFramework_Tests/PsxPtrTests.cs
@@ -0,0 +1,92 @@
+﻿using CTRFramework.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CTRFramework_Tests
+{
+    [TestClass]
+    public class PsxPtrTests
+    {
+        Random random = new Random();
+        List<PsxPtr> initial = new List<PsxPtr>();
+        List<PsxPtr> verify = new List<PsxPtr>();
+        string filenameTest = Path.Combine(Meta.BasePath, "psxptrtest.bin");
+
+        private void GenerateTestData()
+        {
+            Console.WriteLine("Original test pointers:");
+
+            for (int i = 0; i < 4; i++)
+            {
+                PsxPtr ptr = new PsxPtr((uint)random.Next(600000));
+                ptr.ExtraBits = (HiddenBits)i;
+
+                initial.Add(ptr);
+
+                Console.WriteLine(ptr.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Writes pointers to file.
+        /// </summary>
+        [TestMethod]
+        public void PsxPtrWriteTest()
+        {
+            GenerateTestData();
+
+            using (BinaryWriterEx bw = new BinaryWriterEx(File.OpenWrite(filenameTest)))
+            {
+                foreach (var ptr in initial)
+                {
+                    ptr.Write(bw, null);
+                }
+            }
+
+            Assert.IsTrue(File.Exists(filenameTest), "Test file doesn't exist.");
+
+            PsxPtrReadTest();
+        }
+
+        /// <summary>
+        /// Reads pointers from file and compares to the original.
+        /// </summary>
+        public void PsxPtrReadTest()
+        {
+            Console.WriteLine("Verification pointers:");
+
+            using (BinaryReaderEx br = new BinaryReaderEx(File.OpenRead(filenameTest)))
+            {
+                for (int i = 0; i < initial.Count; i++)
+                {
+                    verify.Add(PsxPtr.FromReader(br));
+                }
+            }
+
+            Assert.AreEqual(initial.Count, verify.Count, 0, "Num pointers mismatch.");
+
+            for (int i = 0; i < initial.Count; i++)
+            {
+                Assert.IsTrue(
+                    initial[i] != verify[i],
+                    $"PsxPtr value {i} mismatch: {initial[i].Address} vs {verify[i].Address}, {initial[i].ExtraBits} vs {verify[i].ExtraBits}"
+                );
+
+                Console.WriteLine(verify[i].ToString());
+            }
+
+            CleanUp();
+        }
+
+        private void CleanUp()
+        {
+            initial.Clear();
+            verify.Clear();
+
+            if (File.Exists(filenameTest))
+                File.Delete(filenameTest);
+        }
+    }
+}
