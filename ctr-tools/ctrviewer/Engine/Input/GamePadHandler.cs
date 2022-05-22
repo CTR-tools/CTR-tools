c3fbde05764de5cf152bae7b4429ac03a5e9c741
DCxDemo <ultrataber@gmail.com>
2022-05-22 10:46:26 +0300

    added gamepadhandler
    - added gamepadhandler
    - wrapped all handlers in inputhandler class with gameaction enum

---
+++ ctr-tools/ctrviewer/Engine/Input/GamePadHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace ctrviewer.Engine.Input
+{
+    //maps playstation button names to xbox buttons
+    public enum SonyButtons
+    {
+        Cross = Buttons.A,
+        Circle = Buttons.B,
+        Square = Buttons.X,
+        Triangle = Buttons.Y,
+        R1 = Buttons.RightShoulder,
+        L1 = Buttons.LeftShoulder,
+        R3 = Buttons.RightStick,
+        L3 = Buttons.LeftStick,
+        Start = Buttons.Start,
+        Select = Buttons.Back
+    }
+
+    public static class GamePadHandler
+    {
+        public static PlayerIndex GamePadIndex = PlayerIndex.One;
+
+        private static GamePadState oldState = GamePad.GetState(GamePadIndex);
+        private static GamePadState newState = GamePad.GetState(GamePadIndex);
+
+        public static GamePadState State => newState;
+
+        public static void Update()
+        {
+            if (!GamePad.GetState(GamePadIndex).IsConnected)
+                DetectActiveGamePad();
+
+            oldState = newState;
+            newState = GamePad.GetState(GamePadIndex);
+        }
+
+        public static void Reset()
+        {
+            newState = GamePad.GetState(GamePadIndex);
+            oldState = newState;
+        }
+
+        public static void DetectActiveGamePad()
+        {
+            for (var i = PlayerIndex.One; i <= PlayerIndex.Four; i++)
+                if (GamePad.GetState(i).IsConnected)
+                {
+                    GamePadIndex = i;
+                    break;
+                }
+        }
+
+        public static bool IsPressed(Buttons button) => newState.IsButtonDown(button) && !oldState.IsButtonDown(button);
+        public static bool IsDown(Buttons button) => newState.IsButtonDown(button);
+
+        public static bool AreAllDown(params Buttons[] buttons)
+        {
+            foreach (var button in buttons)
+                if (!IsDown(button))
+                    return false;
+
+            return true;
+        }
+
+        public static bool IsAnyDown(params Buttons[] buttons)
+        {
+            foreach (var button in buttons)
+                if (IsDown(button))
+                    return true;
+
+            return false;
+        }
+
+        public static bool IsComboPressed(Buttons held, Buttons pressed)
+        {
+            return IsDown(held) && IsPressed(pressed);
+        }
+    }
+}
---
+++ ctr-tools/ctrviewer/Engine/Input/InputHandlers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace ctrviewer.Engine.Input
+{
+    public enum GameAction
+    {
+        MenuToggle,
+        MenuConfirm,
+        MenuBack,
+        MenuUp,
+        MenuDown,
+        MenuLeft,
+        MenuRight,
+        MenuEnter,
+        ForceQuit,
+        ToggleConsole
+    }
+
+
+    public static class InputHandlers
+    {
+        public static void Update()
+        {
+            KeyboardHandler.Update();
+            MouseHandler.Update();
+            GamePadHandler.Update();
+        }
+
+        public static bool Process(GameAction action)
+        {
+            switch (action)
+            {
+                case GameAction.MenuToggle: return KeyboardHandler.IsAnyPressed(Keys.Escape) || GamePadHandler.IsPressed((Buttons)SonyButtons.Start);
+
+                case GameAction.MenuUp: return KeyboardHandler.IsAnyPressed(Keys.Up, Keys.W) || GamePadHandler.IsPressed(Buttons.DPadUp);
+                case GameAction.MenuLeft: return KeyboardHandler.IsAnyPressed(Keys.Left, Keys.A) || GamePadHandler.IsPressed(Buttons.DPadLeft);
+                case GameAction.MenuDown: return KeyboardHandler.IsAnyPressed(Keys.Down, Keys.S) || GamePadHandler.IsPressed(Buttons.DPadDown);
+                case GameAction.MenuRight: return KeyboardHandler.IsAnyPressed(Keys.Right, Keys.D) || GamePadHandler.IsPressed(Buttons.DPadRight);
+
+                case GameAction.MenuBack: return KeyboardHandler.IsAnyPressed(Keys.Back) || GamePadHandler.IsPressed((Buttons)SonyButtons.Circle);
+                case GameAction.MenuConfirm: return KeyboardHandler.IsAnyPressed(Keys.Enter) || GamePadHandler.IsPressed((Buttons)SonyButtons.Cross);
+
+                case GameAction.ForceQuit: return (KeyboardHandler.IsAltPressed && KeyboardHandler.IsAnyPressed(Keys.F4)) || GamePadHandler.AreAllDown(Buttons.Start, Buttons.Back);
+                case GameAction.ToggleConsole: return KeyboardHandler.IsPressed(Keys.OemTilde) || GamePadHandler.IsPressed(Buttons.Back);
+            }
+
+            return false;
+        }
+    }
+}
