ba89457661da96606ad91f7349d16f35ca075da7
DCxDemo <ultrataber@gmail.com>
2023-06-11 14:56:20 +0300

    mouse handler refactoring
    mouse handler refactoring

--- ctr-tools/ctrviewer/Engine/Input/MouseHandler.cs
+++ ctr-tools/ctrviewer/Engine/Input/MouseHandler.cs
@@ -5,6 +5,8 @@ namespace ctrviewer.Engine.Input
 {
     public static class MouseHandler
     {
+        #region [Mouse state handling]
+
         private static MouseState oldState = Mouse.GetState();
         private static MouseState newState = Mouse.GetState();
 
@@ -20,17 +22,48 @@ namespace ctrviewer.Engine.Input
             oldState = newState;
         }
 
-        public static bool IsLeftButtonHeld => newState.LeftButton == ButtonState.Pressed;
-        public static bool IsRightButtonHeld => newState.RightButton == ButtonState.Pressed;
-        public static bool IsLeftButtonPressed => newState.LeftButton == ButtonState.Pressed && oldState.LeftButton != ButtonState.Pressed;
-        public static bool IsRightButtonPressed => newState.RightButton == ButtonState.Pressed && oldState.RightButton != ButtonState.Pressed;
-        public static bool IsWheelPressed => newState.MiddleButton == ButtonState.Pressed;
+        #endregion
+
+        #region [Mouse position handling]
         public static int X => newState.X;
         public static int Y => newState.Y;
-        public static Point Position => new Point(X, Y);
+
+        private static Point PositionPoint = new Point(0, 0);
+        public static Point Position
+        {
+            get
+            {
+                PositionPoint.X = X;
+                PositionPoint.Y = Y;
+                return PositionPoint;
+            }
+        }
+
         public static int DeltaX => newState.X - oldState.X;
         public static int DeltaY => newState.Y - oldState.Y;
-        public static Point Delta => new Point(DeltaX, DeltaY);
+
+        private static Point DeltaPoint = new Point(0, 0);
+
+        public static Point Delta
+        {
+            get
+            {
+                DeltaPoint.X = DeltaX;
+                DeltaPoint.Y = DeltaY;
+                return DeltaPoint;
+            }
+        }
+        #endregion
+
+        //held refers to down
+        public static bool IsLeftButtonHeld => newState.LeftButton == ButtonState.Pressed;
+        public static bool IsRightButtonHeld => newState.RightButton == ButtonState.Pressed;
+        public static bool IsWheelHeld => newState.MiddleButton == ButtonState.Pressed;
+
+        //pressed refers to just changed state to down
+        public static bool IsLeftButtonPressed => newState.LeftButton == ButtonState.Pressed && oldState.LeftButton != ButtonState.Pressed;
+        public static bool IsRightButtonPressed => newState.RightButton == ButtonState.Pressed && oldState.RightButton != ButtonState.Pressed;
+        public static bool IsWheelPressed => newState.MiddleButton == ButtonState.Pressed && oldState.MiddleButton != ButtonState.Pressed;
 
         public static bool IsScrollingUp => newState.ScrollWheelValue > oldState.ScrollWheelValue;
         public static bool IsScrollingDown => newState.ScrollWheelValue < oldState.ScrollWheelValue;
