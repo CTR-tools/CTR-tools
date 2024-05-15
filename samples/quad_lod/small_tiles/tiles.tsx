08ac26b08a142797490a05d7ec074885323f4939
DCxDemo <ultrataber@gmail.com>
2024-05-15 21:46:07 +0300

    added quad_lod blend sample
    added quad_lod blend sample

---
+++ samples/quad_lod/small_tiles/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="64" tileheight="64" tilecount="5" columns="5">
+ <image source="../combined_textures/atlas.png" width="320" height="64"/>
+</tileset>
